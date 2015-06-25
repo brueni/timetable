b6325df2672f3ae6a17a794649965d788bee22de
Michael Brünisholz <info@michael-bruenisholz.ch>
2015-06-25 20:24:30 +0200

    insert function

--- shifts.php
+++ shifts.php
@@ -1,3 +1,18 @@
+<?php
+if ($_GET['do'] == "new") {
+	$newline = array(
+		$_GET['name'],
+		$_GET['start1'],
+		$_GET['end1'],
+		$_GET['start2'],
+		$_GET['end2']
+	);
+	$f = fopen("shifts.csv.default", "a");
+	fputcsv($f, $newline);
+	fclose($f);
+}
+?>
+
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
 	"http://www.w3.org/TR/html4/strict.dtd">
 <html>
@@ -25,7 +40,7 @@
 				        foreach ($line as $cell) {
 				                echo "<td>" . htmlspecialchars($cell) . "</td>";
 				        }
-				        echo "<td><a href=\"?do=delete&line=" . $i . "\">L&ouml;schen</a></td></tr>\n";
+				        echo "<td><a href=\"?do=delete&line=" . $n . "\">L&ouml;schen</a></td></tr>\n";
 						$n++;
 				}
 				fclose($f);
@@ -34,7 +49,7 @@
 				<td colspan="7">Neuer Dienst einf&uuml;gen</td>
 			</tr>
 			<tr>
-				<form action="?do=add" method="get">
+				<form action="#" method="get">
 				<td></td>
 				<td><input type="text" name="name" /></td>
 				<td><input type="text" name="start1" /></td>
@@ -42,7 +57,7 @@
 				<td><input type="text" name="start2" /></td>
 				<td><input type="text" name="end2" /></td>
 				<td><input type="submit" name="submit" value="Eintragen" /></td>
-				</form>
+				<input type="hidden" name="do" value="new" /></form>
 			</tr>
 		</table>
 	</body>
