ffcd107af83f2fda650c35cb63930a12cd4ce761
Michael Brünisholz <info@michael-bruenisholz.ch>
2015-06-25 20:00:28 +0200

    Basics

--- shifts.php
+++ shifts.php
@@ -6,15 +6,17 @@
 	</head>
 	<body>
 				<h1>M&ouml;gliche Schichten</h1>
-		<table>
-			<th>
+				<a href="index.php">Zur&uuml;ck</a>
+		<table border="1">
+			<tr>
 				<td>ID</td>
 				<td>Name</td>
 				<td>Startzeit 1</td>
 				<td>Endzeit 1</td>
 				<td>Startzeit 2</td>
 				<td>Endzeit 2</td>
-			</th>
+				<td>Aktion</td>
+			</tr>
 			<?php
 				$n = "1";
 				$f = fopen("shifts.csv.default", "r");
@@ -23,11 +25,25 @@
 				        foreach ($line as $cell) {
 				                echo "<td>" . htmlspecialchars($cell) . "</td>";
 				        }
-				        echo "</tr>\n";
+				        echo "<td><a href=\"?do=delete&line=" . $i . "\">L&ouml;schen</a></td></tr>\n";
 						$n++;
 				}
 				fclose($f);
 			?>
+			<tr>
+				<td colspan="7">Neuer Dienst einf&uuml;gen</td>
+			</tr>
+			<tr>
+				<form action="?do=add" method="get">
+				<td></td>
+				<td><input type="text" name="name" /></td>
+				<td><input type="text" name="start1" /></td>
+				<td><input type="text" name="end1" /></td>
+				<td><input type="text" name="start2" /></td>
+				<td><input type="text" name="end2" /></td>
+				<td><input type="submit" name="submit" value="Eintragen" /></td>
+				</form>
+			</tr>
 		</table>
 	</body>
 </html>
