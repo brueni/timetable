d5c8e39cd14781c99c28494031f9915cc122df56
Michael Brünisholz <info@michael-bruenisholz.ch>
2015-07-08 21:19:58 +0200

    Show days with weekday, prepared input-select field

--- create_detail.php
+++ create_detail.php
@@ -1,3 +1,4 @@
+<?php include 'include.php' ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
 	"http://www.w3.org/TR/html4/strict.dtd">
 <html>
@@ -7,12 +8,30 @@
 	<body>
 				<h1>Monat <?php echo $_GET['y'] . "/" . $_GET['m']; ?> erfassen</h1>
 				<a href="create.php">Zur&uuml;ck</a>
+				<table border=1>
+					<tr>
+						<td>Datum</td>
+						<td>Wochentag</td>
+						<td>Dienst</td>
+					</tr>
 				<?php
 				$start_of_month = mktime(0, 0, 1, $_GET['m'], 1, $_GET['y']);
 				$days_in_month = date(t, $start_of_month);
-				echo $start_of_month;
-				echo "<br>";
-				echo $days_in_month;
+				$i = "1";
+				while ($i <= $days_in_month) {
+					$weekday = date('D', mktime(0,0,1,$_GET['m'], $i, $_GET['y']));
+					$weekday = strtr($weekday, $trans);
+					echo "<tr>
+							<td>$i.</td>
+							<td>$weekday</td>
+							<td><select name=\"dienst_$i\">
+									<opton value=\"foo\">bar</option>
+								</select>
+							</td>
+						</tr>";
+					$i++;
+				}
 				?>
+				</table>
 	</body>
 </html>
