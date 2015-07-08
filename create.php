7a8abaea05ef81c1b2f9fb4911f6bc03a52b5ad7
Michael Brünisholz <info@michael-bruenisholz.ch>
2015-07-08 20:40:14 +0200

    bug

--- create.php
+++ create.php
@@ -9,16 +9,16 @@
 				<a href="index.php">Zur&uuml;ck</a>
 				<?php
 				$month_now = date(m);
-				$month_plus1 = date(m) + 1;
-				$month_plus2 = date(m) + 2;
+				$month_plus1 = date(n) + 1;
+				$month_plus2 = date(n) + 2;
 				$year_now = date(Y);
 				if ($month_plus1 == 13) {$month_plus1 = 1; $year_now++;}
 				if ($month_plus2 == 13) {$month_plus2 = 1; $year_now++;}
 				?>
 				<ul>
-					<li><a href="create_detail.php?y=$year_now&$m=$month_now"><?php echo $year_now . "/" . $month_now; ?></a></li>
-					<li><a href="create_detail.php?y=$year_now&$m=$month_plus1"><?php echo $year_now . "/" . $month_plus1; ?></a></li>
-					<li><a href="create_detail.php?y=$year_now&$m=$month_plus1"><?php echo $year_now . "/" . $month_plus2; ?></a></li>
+					<li><a href="create_detail.php?y=<?php echo $year_now . "&m=" . $month_now; ?>"><?php echo $year_now . "/" . $month_now; ?></a></li>
+					<li><a href="create_detail.php?y=<?php echo $year_now . "&m=" . $month_plus1; ?>"><?php echo $year_now . "/" . $month_plus1; ?></a></li>
+					<li><a href="create_detail.php?y=<?php echo $year_now . "&m=" . $month_plus2; ?>"><?php echo $year_now . "/" . $month_plus2; ?></a></li>
 				</ul>
 	</body>
 </html>
