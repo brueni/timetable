576f9999200e7d2b378731057e659925b4b2536f
Michael Brünisholz <info@michael-bruenisholz.ch>
2015-07-20 20:36:47 +0200

    generate ics out of every txt file

--- generate_ics.php
+++ generate_ics.php
@@ -1,35 +1,21 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
-        "http://www.w3.org/TR/html4/strict.dtd">
-
-
 <?php include 'include.php';
-$fileheader = "BEGIN:VCALENDAR\n
+$dir="data/";
+$ics = "BEGIN:VCALENDAR\n
 VERSION:2.0\n
 PRODID:-//hacksw/handcal//NONSGML v1.0//EN\n";
-$filefooter = "END:VCALENDAR";
+
 
 if ($handle = opendir('data')) {
-    echo "Verzeichnis-Handle: $handle\n";
-    echo "Einträge:\n";
-
-    /* Das ist der korrekte Weg, ein Verzeichnis zu durchlaufen. */
     while (false !== ($entry = readdir($handle))) {
         if($entry != "." && $entry != "..") {
-        	echo "$entry\n";	
+        	$ics .= readfile($entry);	
         }	
     }
     closedir($handle);
 }
+$ics .= "END:VCALENDAR";
 
+$file = fopen("data/shifts.ics", "w");
+fwrite($file, $ics);
+close($file);
 ?>
-
-
-
-<html>
-        <head>
-                <title></title>
-        </head>
-        <body>
-		
-	</body>
-</html>
