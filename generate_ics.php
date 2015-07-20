f3ea986133da5ada2362506e63bae148dbb560ca
Michael Brünisholz <info@michael-bruenisholz.ch>
2015-07-20 20:40:44 +0200

    bug

--- generate_ics.php
+++ generate_ics.php
@@ -2,13 +2,13 @@
 $dir="data/";
 $ics = "BEGIN:VCALENDAR\n
 VERSION:2.0\n
-PRODID:-//hacksw/handcal//NONSGML v1.0//EN\n";
+PRODID:-//Michael Bruenisholz//Timetable//EN\n";
 
 
 if ($handle = opendir('data')) {
     while (false !== ($entry = readdir($handle))) {
         if($entry != "." && $entry != "..") {
-        	$ics .= readfile($entry);	
+        	$ics .= readfile($dir . $entry);	
         }	
     }
     closedir($handle);
