d31ca4eecc978816abd003a3a759bc73c67bc848
Michael Brünisholz <info@michael-bruenisholz.ch>
2015-07-23 21:38:02 +0200

    Timezone-Settings

--- generate_ics.php
+++ generate_ics.php
@@ -3,7 +3,24 @@ $dir="data/";
 $eol = "\n";
 $ics = "BEGIN:VCALENDAR" . $eol . 
 "VERSION:2.0" . $eol . 
-"PRODID:-//MichaelBruenisholz//Timetable//EN" . $eol;
+"PRODID:-//MichaelBruenisholz//Timetable//EN" . $eol .
+"BEGIN:VTIMEZONE" . $eol .
+"TZID:Europe/Zurich" . $eol .
+"BEGIN:DAYLIGHT" . $eol .
+"TZOFFSETFROM:+0100" . $eol .
+"TZOFFSETTO:+0200" . $eol .
+"DTSTART:19810329T020000" . $eol .
+"RRULE:FREQ=YEARLY;BYMONTH=3;BYDAY=-1SU" . $eol .
+"TZNAME:CEST" . $eol .
+"END:DAYLIGHT" . $eol .
+"BEGIN:STANDARD" . $eol .
+"TZOFFSETFROM:+0200" . $eol .
+"TZOFFSETTO:+0100" . $eol .
+"DTSTART:19961027T030000" . $eol .
+"RRULE:FREQ=YEARLY;BYMONTH=10;BYDAY=-1SU" . $eol .
+"TZNAME:CET" . $eol .
+"END:STANDARD" . $eol .
+"END:VTIMEZONE" . $eol;
 
 if ($handle = opendir('data')) {
     while (false !== ($entry = readdir($handle))) {
